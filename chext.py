f8189e8366a5b424edf7f1faae8e700cf14b5861
Gustavo Loaiza <loaizagustavo95@gmail.com>
2017-11-20 12:42:40 -0600

    Revisar extensión y path

--- chext.py
+++ chext.py
@@ -1,27 +1,33 @@
 import os
-from os.path import isfile, join, isdir
+from os.path import isfile, join, isdir, splitext
 from os import listdir
 
+def cambiar_ext(path, ext):
+    path = safe_path(path)
+    ext = safe_ext(ext)
+
+    if isdir(path):
+        for file in listdir(path):
+            if isfile(join(path, file)):
+                base = splitext(file)[0]
+                os.rename(path + file, path +  base + ext)
+        return "Listo"
+    else:
+        return path + " no es un directorio válido"
+
+def safe_path(path):
+    path += "\\"
+    return path
+
+def safe_ext(ext):
+    ext = ext.replace(".", "")
+    ext = "." + ext
+    return ext
 
 def main():
     path = input("Ingrese la ruta absoluta del directorio que contiene los archivos: ")
-    if isdir(path):
-        path = path + "\\"
-        ext = input("A que extension quiere cambiar: ")
-        ext = "." + ext
-        for file in listdir(path):
-            if isfile(join(path, file)):
-                base = os.path.splitext(file)[0]
-                os.rename(path + file, path +  base + ext)
+    ext = input("A que extension quiere cambiar: ")
 
-        print("Listo!")
-    else:
-        print ("La ruta no apunta a un directorio válido")
-
-def isdir(path):
-    if os.path.isdir(path):
-        return True
-    else:
-        return False
+    print(cambiar_ext(path, ext))
 
 main()
